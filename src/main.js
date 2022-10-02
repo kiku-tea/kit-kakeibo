1083c02e208cbdef415f8286c6cead07478cc780
hidaken991018 <ed.hidaken@gmail.com>
2022-10-02 10:51:40 +0900

    リセットCSS初期設定

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueCookies from 'vue-cookies'
+require('@/assets/common/scss/home.scss')
 
 createApp(App).use(store).use(router).use(VueCookies).mount('#app')
