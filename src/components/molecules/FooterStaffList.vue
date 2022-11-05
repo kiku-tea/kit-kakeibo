dda52896c664a68311e8c8523bfc342b77d2176d
Takuya Kikuchi <kikutea330@gmail.com>
2022-11-05 09:57:02 +0900

    RAGEBLUEのフッターのワイヤー追加

---
+++ src/components/molecules/FooterStaffList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul >
+    <li v-for="(staff, index) in staffList" :key="index" >
+      <span>{{staff}}</span>
+    </li>
+  </ul>
+</template>
+<script>
+  export default{
+    data(){
+      return {
+        staffList: [
+          'Photo & Movie_Jun Okada［BE NATURAL］',
+          'Styling_Tomoya Yagi',
+          'Hair & Make-up_Yuka Toyama［mod’s hair］',
+          'Web Development_Yusuke Fukunaga'
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/components/organisms/FooterView.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <FooterStaffList></FooterStaffList>
+    <FooterBottun>{{text}}</FooterBottun>
+  </footer>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import FooterBottun from '@/components/atoms/FooterBottun.vue';
+import FooterStaffList from '@/components/molecules/FooterStaffList.vue';
+
+export default defineComponent({
+  name: 'FooterView',
+  components:{
+    FooterBottun,
+    FooterStaffList
+},
+  data: function(){
+    return {
+      text: 'BRAND TOP'
+    };
+  }
+});
+</script>
