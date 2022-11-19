7be0db31fa55fff7d3c72dca0eac76ac31a935a9
Takuya Kikuchi <kikutea330@gmail.com>
2022-11-19 13:01:41 +0900

    modified: Index Template (add bottom-section and footer-section)

--- src/components/rageblue/templates/RagablueIndex.vue
+++ src/components/rageblue/templates/RagablueIndex.vue
@@ -1,12 +1,48 @@
 <template>
     <p>RagableuIndex</p>
-    <PCNav/>
+    <main class="main">
+        <div class="main__left-container">
+            <PCNav/>
+        </div>
+        <div class="main__right-container">
+            <BottomView></BottomView>
+            <FooterView></FooterView>
+        </div>
+    </main>
 </template>
 <script>
  import PCNav from '../organisms/PCNav.vue';
+ import BottomView from '../organisms/BottomView.vue';
+ import FooterView from '../organisms/FooterView.vue';
  export default{
     components:{
         PCNav,
+        BottomView,
+        FooterView
     }
  }
 </script>
+<style scoped>
+.main{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+.main > div{
+    flex-basis: 100%;
+}
+.main .main__right-container{
+    margin: 0;
+    padding: 0;
+}
+@media screen and (min-width: 769px){
+    .main > div{
+        flex-basis: 50%;
+    }
+    .main .main__right-container{
+        width: 375px;
+        margin: auto;
+        max-width: 375px;
+    }
+}
+</style>
