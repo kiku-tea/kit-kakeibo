e2503cf23ce118cc066c9c31919734e8a73672a2
Takuya Kikuchi <kikutea330@gmail.com>
2022-12-18 10:01:15 +0900

    add atoms - section divider

---
+++ src/components/rageblue/atoms/SectionDivider.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="divider"></div>
+</template>
+<script setup>
+</script>
+<style scoped>
+.divider{
+  position: relative;
+  padding-top: 26.8%;
+  margin-top: 10.8%;
+}
+.divider::before{
+  content: '';
+  position: absolute;
+  top: 0;
+  width: 104%;
+  height: 1px;
+  background-color: black;
+}
+.divider:nth-of-type(2n)::before{
+  left: 0;
+  transform-origin: left;
+  transform: rotate(15deg);
+}
+.divider:nth-of-type(2n+1)::before{
+  right: 0;
+  transform-origin: right;
+  transform: rotate(-15deg);
+}
+</style>
