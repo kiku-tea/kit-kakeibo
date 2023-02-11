2c28a74a2ea97bb741de92cea63770ec9dc2513b
Takuya Kikuchi <kikutea330@gmail.com>
2023-02-11 10:18:35 +0900

    LookIntro.HeadingOccupation: add animation

--- src/components/rageblue/molecules/LookIntro.vue
+++ src/components/rageblue/molecules/LookIntro.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, onBeforeMount } from 'vue';
+import { ref, defineProps, onBeforeMount, onMounted, onBeforeUnmount } from 'vue';
 import RandomChar from "../atoms/RandomChar.vue";
 const props = defineProps({
   num: Number,
@@ -15,6 +15,25 @@ onBeforeMount(()=>{
   delay.value = 1000 / (props.firstName.length + props.familyName.length);
 });
 
+let once = false;
+const headingOcc = ref(); //当コンポーネントのDOMアクセス用
+const onScroll = ()=>{
+  console.log(headingOcc.value);
+  const targetPosition = headingOcc.value.getBoundingClientRect().top;
+  if(targetPosition <= window.innerHeight && !once){
+    once = true;
+    headingOcc.value.classList.add('is-show');
+  }
+}
+
+onMounted(()=>{
+  window.addEventListener('scroll', onScroll);
+  onScroll();
+});
+onBeforeUnmount(()=>{
+  window.removeEventListener('scroll', onScroll);
+});
+
 </script>
 <template>
   <div class="look-intro">
@@ -27,7 +46,7 @@ onBeforeMount(()=>{
         <RandomChar v-for="char,i in familyName.split('')" :key="i"
           :char="char" :time="500" :delay="(firstName.length*delay + i*delay)"></RandomChar>
       </p>
-      <p class="look-intro__heading-occupation">{{occupation}}</p>
+      <p class="look-intro__heading-occupation" ref="headingOcc"><span>{{occupation}}</span></p>
     </h3>
     <p class="look-intro__lead"><slot></slot></p>
     <div class="look-intro__insta">
@@ -39,7 +58,7 @@ onBeforeMount(()=>{
 </template>
 <style scoped>
 .look-intro{
-
+  text-align: center;
 }
 .look-intro__insta-anchor{
   position: relative;
@@ -65,5 +84,51 @@ onBeforeMount(()=>{
   background-repeat: no-repeat;
   background-size: cover;
   background-image: url("@/assets/img/svg/ico_insta.svg");
+}
+
+.look-intro__heading-occupation{
+  font-size: 12px;
+  display: inline-block;
+  position: relative;
+  margin-top: 2.67%;
+  font-weight: 600;
+}
+.look-intro__heading-occupation span{
+  opacity: 0;
+}
+.look-intro__heading-occupation.is-show span{
+  opacity: 1;
+}
+.look-intro__heading-occupation::after{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  transform: scaleX(0) translate3d(0,0,0);
+}
+.look-intro__heading-occupation.is-show::after{
+  animation: look_heading_occu 700ms cubic-bezier(0.19, 1, 0.22, 1) forwards;
+}
+@keyframes look_heading_occu {
+  0% {
+    transform: scaleX(0) translate3d(0,0,0);
+    transform-origin: left;
+  }
+  50% {
+    transform-origin: left;
+  }
+  51% {
+    transform-origin: right;
+  }
+  50%,55%{
+    transform: scaleX(1) translate3d(0,0,0);
+  }
+  100% {
+    transform: scaleX(0) translate3d(0,0,0);
+    transform-origin: right;
+  }
 }
 </style>
